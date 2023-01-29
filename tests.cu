6003f8bff77377104455e420120eb8d7ac9b3a2f
RaulPPealez <raulppelaez@gmail.com>
2023-01-29 19:03:45 +0100

    Add more tests

--- tests.cu
+++ tests.cu
@@ -1,3 +1,6 @@
+/* Raul P. Pelaez 2023. Tests for the dry wet slit channel Integrator
+
+ */
 #include <gtest/gtest.h>
 #include "gmock/gmock.h"
 #include <random>
@@ -65,8 +68,6 @@ TEST(FullDryMobility, SelfMobilityIsCorrect){
   ASSERT_THAT(M0, ::testing::DoubleNear(1, 1e-5));
 }
 
-
-
 TEST(FullWetMobility, SelfMobilityIsCorrectAtMiddlePlaneForLargeDomain){
   using BD = DryWetBD;
   writeDefaultMobilityFile();
@@ -87,4 +88,40 @@ TEST(FullWetMobility, SelfMobilityIsCorrectAtMiddlePlaneForLargeDomain){
   bd->forwardTime();
   real M0 = pd->getPos(access::cpu, access::write)[0].x;
   ASSERT_THAT(M0, ::testing::DoubleNear(1, 1e-2));
+}
+
+
+//Asserts the correctness of the self mobility for a certain wet radius
+//All other parameters are hardcoded, see the function.
+//The total hydrodynamic radius is 1 (meaning that wetRadius >=1 is full wet)
+void computeSelfMobilityWithWetRadius(real wetRadius){
+  using BD = DryWetBD;
+  writeDefaultMobilityFile();
+  BD::Parameters par;
+  par.temperature = 0;
+  par.viscosity = 1.0/(6*M_PI);
+  par.hydrodynamicRadius = 1.0;
+  par.dt = 1.0;
+  par.wetRadius = wetRadius;
+  par.brownianUpdateRule = DryWetBD::update_rules::euler_maruyama;
+  par.dryMobilityFile = "mob.dat";
+  par.H = 128;
+  par.Lxy = 64;
+  auto pd = std::make_shared<ParticleData>(1);
+  pd->getPos(access::cpu, access::write)[0] = real4();
+  auto bd = std::make_shared<BD>(pd, par);
+  bd->addInteractor(std::make_shared<miniInteractor>(pd));
+  bd->forwardTime();
+  real M0 = pd->getPos(access::cpu, access::write)[0].x;
+  ASSERT_THAT(M0, ::testing::DoubleNear(1, 1e-1))<<"Failed with wet radius "<<wetRadius;
+}
+
+TEST(DryWetMobility, SelfMobilityIsCorrectForAnyWetRadius){
+  real minWetRadius = 0.1;
+  real maxWetRadius = 0.9;
+  int Ntest = 8;
+  fori(0, Ntest){
+    real wetRadius = minWetRadius + i*(maxWetRadius - minWetRadius)/(Ntest-1);
+    computeSelfMobilityWithWetRadius(wetRadius);
+  }
 }
