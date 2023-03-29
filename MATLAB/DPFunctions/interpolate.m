57c1effa22c261d9c7806500dc3285e32690e8c5
rfjd <aref.hashemi@gmail.com>
2023-03-29 16:02:18 -0400

    further cleaning

--- MATLAB/DPFunctions/interpolate.m
+++
@@ -1,3 +0,0 @@
-function pPtsDP = interpolate(I,pgrid,nxy,Nz)
-    pPtsDP=I'*reshape(permute(pgrid,[2 1 3]),nxy*nxy*Nz,1);
-end
--- MATLAB/Poisson/TriplyPeriodic/spread.m
+++
@@ -1,4 +0,0 @@
-function gridfx = spread(S,forces,N)
-    gridf=full(S*forces);
-    gridfx=permute(reshape(gridf(:,1),nxy,nxy,Nz),[2 1 3]);
-end
